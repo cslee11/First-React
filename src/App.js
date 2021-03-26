d0b8e799b3809b2235371cfe1c2ec765c72580ed
cslee11 <75759362+cslee11@users.noreply.github.com>
2021-03-26 14:37:29 -0500

    redo folders

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          abcdefg
+          First React Project - Chia
         </a>
       </header>
     </div>
