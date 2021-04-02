17836b4d3ba5606d8b608dcb16019fd1b1d27cba
cslee11 <75759362+cslee11@users.noreply.github.com>
2021-04-02 06:19:43 -0500

    deleted broken things

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
   
 import React from "react";
 import ReactDOM from "react-dom";
-import Navbar from "./components/Navbar/NavBar";
 import Home from "./pages/Home/Home";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Nav/>
+    <Home/>
   </React.StrictMode>,
   document.getElementById("root")
 );
